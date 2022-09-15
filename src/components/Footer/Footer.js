d2c0a76047ea1dff3fac022f99aa07cf45956b55
DIWAKAR SINGH <95065151+diwakars652@users.noreply.github.com>
2022-09-15 21:29:42 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="py-5 bg-dark">
             <div className="container">
-                <p className="m-0 text-center text-white">Copyright © Emre Baskan 2019</p>
+                <p className="m-0 text-center text-white">Copyright © Diwakar Singh 2022</p>
             </div>
         </footer>
     );
